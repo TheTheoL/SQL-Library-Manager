8511b7f12f3193861191c4a9bd9aa51d3b7539ba
Theo L <atlufkin@outlook.com>
2022-01-12 13:54:03 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,29 @@
 var express = require('express');
 var router = express.Router();
-const Books = require('../models/books.js').Books 
+const Book = require('../models').Book;
+
+/* Handler function to wrap each route. */
+function asyncHandler(cb){
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch(error){
+      res.status(500).send(error);
+      next(error);
+    }
+  }
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', asyncHandler(async (req, res) => {
   // res.render('index', { title: 'Express' });
-  const allBooks = await Books.findAll();
-});
+  const allBooks = await Book.findAll();
+  
+}));
 console.log(allBooks.toJSON());
 module.exports = router;
+
+
+
+
+
