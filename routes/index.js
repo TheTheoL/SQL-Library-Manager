c74689279cb412a8bbc8f7c16e92df23514b3315
Theo L <atlufkin@outlook.com>
2022-01-15 15:01:47 -0500

    Updated

--- routes/index.js
+++ routes/index.js
@@ -43,15 +43,15 @@ router.get('/books/new', asyncHandler(async (req, res) => {
 
 //POST New book to database 
 router.post('/books/new', asyncHandler(async (req, res) => {
-  let newBook;
+  let book;
   try {
-    newBook = await Book.create(req.body);
+    book = await Book.create(req.body);
     res.redirect("/");
   
   } catch (error) {
     if(error.name === "SequelizeValidationError") { 
-      newBook = await Book.create(req.body);
-      res.render("new-books", { newBook, errors: error.errors, title: "New Book" })
+      book = await Book.build(req.body);
+      res.render("new-books", { book, errors: error.errors, title: "New Book" })
     } else {
       throw error; 
     }  
@@ -60,10 +60,10 @@ router.post('/books/new', asyncHandler(async (req, res) => {
 
 // /books.:id to show book detail form
 router.get('/books/:id', asyncHandler(async (req, res) => {
-  const books = await Book.findByPk(req.params.id);
+  const book = await Book.findByPk(req.params.id);
 
-  if(books) {
-      res.render('update-book', { title: books.title, books } );
+  if(book) {
+      res.render('update-book', { title: book.title, books } );
   }else {
     res.sendStatus(404);
   }
@@ -71,16 +71,16 @@ router.get('/books/:id', asyncHandler(async (req, res) => {
 
 //Route that updates book info in the database 
 router.get('/books/:id', asyncHandler(async (req, res) => {
-  const books = await Book.findByPk(req.params.id);
+  const book = await Book.findByPk(req.params.id);
 
   try {
-    await books.update(req.body);
+    await book.update(req.body);
     res.redirect('/books');
   } catch (error) {
     if(error.name === "SequelizeValidationError") {
-      const books = await Book.create(req.body);
-      books.id = req.params.id; // make sure correct article gets updated
-      res.render("update-book", { books, errors: error.errors, title: "Update Book" });
+      const book = await Book.build(req.body);
+      book.id = req.params.id; // make sure correct article gets updated
+      res.render("update-book", { book, errors: error.errors, title: "Update Book" });
     } else {
       throw error;
     }
@@ -89,10 +89,10 @@ router.get('/books/:id', asyncHandler(async (req, res) => {
 
 //POST the page to delete books from the database. CANNOT BE UNDONE
 router.post('/books/:id/delete', asyncHandler(async (req, res) => {
-  const books = await Book.findByPk(req.params.id);
+  const book = await Book.findByPk(req.params.id);
   
         try {
-            await books.destroy();
+            await book.destroy();
             res.redirect('/');
     } catch (error) {
       throw error;
@@ -100,26 +100,32 @@ router.post('/books/:id/delete', asyncHandler(async (req, res) => {
   
 }));
 
-//POST an updated book to the database.
-// router.post('/books/:id', asyncHandler(async(req, res) => {
-//   let updateBook;
-//   try{
-//     updateBook = await Books.findByPk(req.params.id);
-//     if(updateBook) {
-//     await books.update(req.body);
-//     res.redirect("/abooks/" + books.id); 
-//     res.redirect("/");
-//     } else {
-//       res.sendStatus(404);
-//     }
-//   } catch (error) {
-//     if (error.name === 'SequelizeValidationError') {
-//       updateBook = await Books.build(req.body);
-//       books.id = req.params.id;
-//       res.render('update-book', { updateBook, errors: error.errors, title: 'Update Book'});
-//     } else {
-//       throw error;
-//     }
-//   }
-// }));
+//ERROR HANDLERS
+
+//404
+router.use((req, res, next) => {
+  const error = new Error();
+  error.status = 404;
+  error.message = 'Page Not Found';
+  console.log(error.status, error.message);
+  res.status(404).render('page-not-found', { error });
+  next(error);
+});
+
+//Global
+router.use((error, req, res, next) => {
+  if (error) {
+    console.log('Global error handler called', error);
+  }
+  if (error.status === 404){
+   res.status(404).render('page-not-found', { error });
+  } else {
+   error.status = 500;
+   error.message = 'Something Went Wrong';
+   console.log(error.message, error.status);
+   res.status(500).render('error', { error });
+  }
+});
+
+
 module.exports = router;
