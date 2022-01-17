2746d33ceda55164066aa5762d6e10268a940c48
Theo L <atlufkin@outlook.com>
2022-01-17 13:44:51 -0500

    Update
    Fixed my routes, errors not rendering right, and added new code so the delete function of the app works.

--- app.js
+++ app.js
@@ -26,12 +26,16 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-app.set('view options', { pretty: true });
+app.set('view options', {
+  pretty: true
+});
 app.use('/static', express.static('public'));
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -39,12 +43,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -56,12 +60,12 @@ app.use(function(err, req, res, next) {
 
 
 /* ERROR HANDLERS */
-/* 404 handler to catch undefined or non-existent route requests */ 
+/* 404 handler to catch undefined or non-existent route requests */
 app.use((req, res, next) => {
   console.log('404 error handler called');
 
-  
- res.status(404).render('error');
+
+  res.status(404).render('error');
 });
 
 /* Global error handler */
@@ -71,11 +75,15 @@ app.use((err, req, res, next) => {
     console.log('Global error handler called', err);
   }
 
-    if (err.status === 404) {
-    res.status(404).render('page-not-found', { err });
+  if (err.status === 404) {
+    res.status(404).render('page-not-found', {
+      err
+    });
   } else {
     err.message = err.message || `Oops! Something went wrong with the server`;
-    res.status(err.status || 500).render('error', { err });
+    res.status(err.status || 500).render('error', {
+      err
+    });
   }
 });
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,11 @@ var router = express.Router();
 
 
 /* Handler function to wrap each route. */
-function asyncHandler(cb){
-  return async(req, res, next) => {
+function asyncHandler(cb) {
+  return async (req, res, next) => {
     try {
       await cb(req, res, next)
-    } catch(error){
+    } catch (error) {
       res.status(500).send(error);
       next(error);
     }
@@ -22,23 +22,28 @@ function asyncHandler(cb){
 
 //Home route
 router.get('/', asyncHandler(async (req, res) => {
-    res.redirect("/books");
+  res.redirect("/books");
 }));
 
 //Route for /books
 router.get('/books', asyncHandler(async (req, res) => {
-    const books = await Book.findAll({});
+  const books = await Book.findAll({});
 
-    if(books) {
-      res.render('index', { books });
-    } else {
-      res.status(404).render('error');
-    }
+  if (books) {
+    res.render('index', {
+      books
+    });
+  } else {
+    res.status(404).render('error');
+  }
 }));
 
 //Route for books/new that renders the new-books template
 router.get('/books/new', asyncHandler(async (req, res) => {
-  res.render('new-books', { title: 'New Book', book: {} });
+  res.render('new-books', {
+    title: 'New Book',
+    book: {}
+  });
 }));
 
 //POST New book to database 
@@ -47,14 +52,18 @@ router.post('/books/new', asyncHandler(async (req, res) => {
   try {
     const book = await Book.create(req.body);
     res.redirect("/");
-  
+
   } catch (error) {
-    if(error.name === "SequelizeValidationError") { 
+    if (error.name === "SequelizeValidationError") {
       book = await Book.build(req.body);
-      res.render("new-books", { book, errors: error.errors, title: "New Book" })
+      res.render("new-books", {
+        book,
+        errors: error.errors,
+        title: "New Book"
+      })
     } else {
-      throw error; 
-    }  
+      throw error;
+    }
   }
 }));
 
@@ -62,9 +71,12 @@ router.post('/books/new', asyncHandler(async (req, res) => {
 router.get('/books/:id', asyncHandler(async (req, res, next) => {
   const book = await Book.findByPk(req.params.id);
 
-  if(book) {
-      res.render('update-book', { title: book.title, book } );
-  }else {
+  if (book) {
+    res.render('update-book', {
+      title: book.title,
+      book
+    });
+  } else {
     const err = new Error();
     err.status = 404;
     err.message = 'Sorry, cannot find book.';
@@ -80,10 +92,14 @@ router.post('/books/:id', asyncHandler(async (req, res, next) => {
     await book.update(req.body);
     res.redirect('/');
   } catch (error) {
-    if(error.name === "SequelizeValidationError") {
+    if (error.name === "SequelizeValidationError") {
       const book = await Book.build(req.body);
       book.id = req.params.id; // make sure correct article gets updated
-      res.render("update-book", { title: 'Update Book', book, errors: error.errors});
+      res.render("update-book", {
+        title: 'Update Book',
+        book,
+        errors: error.errors
+      });
       next(error);
     } else {
       throw error;
@@ -94,14 +110,14 @@ router.post('/books/:id', asyncHandler(async (req, res, next) => {
 //POST the page to delete books from the database. CANNOT BE UNDONE
 router.post('/books/:id/delete', asyncHandler(async (req, res) => {
   const book = await Book.findByPk(req.params.id);
-  
-        try {
-            await book.destroy();
-            res.redirect('/');
-    } catch (error) {
-      throw error;
-    }   
-  
+
+  try {
+    await book.destroy();
+    res.redirect('/');
+  } catch (error) {
+    throw error;
+  }
+
 }));
 
 //ERROR HANDLERS
@@ -112,20 +128,26 @@ router.use((req, res, next) => {
   error.status = 404;
   error.message = 'Sorry, page does not exist.';
   console.log(error.status, error.message);
-  res.status(404).render('page-not-found', { error });
+  res.status(404).render('page-not-found', {
+    error
+  });
   next(error);
 });
 
 //Global error handler
 router.use((error, req, res, next) => {
-  
-  if (error.status === 404){
-   res.status(404).render('page-not-found', { error });
+
+  if (error.status === 404) {
+    res.status(404).render('page-not-found', {
+      error
+    });
   } else {
-   error.status = 500;
-   error.message = 'Oh no! Server is having issues with that';
-   console.log(error.message, error.status);
-   res.status(500).render('error', { error });
+    error.status = 500;
+    error.message = 'Oh no! Server is having issues with that';
+    console.log(error.message, error.status);
+    res.status(500).render('error', {
+      error
+    });
   }
 });
 
